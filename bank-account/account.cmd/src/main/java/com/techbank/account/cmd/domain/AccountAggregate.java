bab9713d7e618bfdaf6de95812bc1afaeaebe600
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-14 12:13:11 +0700

    create AccountAgregate extends from AggregateRoot

---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/domain/AccountAggregate.java
@@ -0,0 +1,87 @@
+package com.techbank.account.cmd.domain;
+
+import com.techbank.account.cmd.api.commands.OpenAccountCommand;
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawEvent;
+import com.techbank.cqrs.core.domain.AggregateRoot;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+public class AccountAggregate extends AggregateRoot {
+    private Boolean active;
+    private double balance;
+
+    public AccountAggregate(OpenAccountCommand command) {
+        raiseEvent(AccountOpenedEvent.builder()
+                .id(command.getId())
+                .accountHolder(command.getAccountHolder())
+                .createdDate(new Date())
+                .accountType(command.getAccountType())
+                .openingBalance(command.getOpeningBalance())
+                .build());
+    }
+
+    public void apply(AccountOpenedEvent event) {
+        this.id = event.getId();
+        this.active = true;
+        this.balance = event.getOpeningBalance();
+    }
+
+    public void depositFunds(double amount) {
+        if (!this.active) {
+            throw new IllegalStateException("Funds cannot be deposited into a closed account!");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalStateException("The deposit amount must be greater than 0!");
+        }
+        raiseEvent(FundsDepositedEvent.builder()
+                .id(this.id)
+                .amount(amount)
+                .build());
+    }
+
+    public void apply(FundsDepositedEvent event) {
+        this.id = event.getId();
+        this.balance += event.getAmount();
+    }
+
+    public void withdrawFunds(double amount) {
+        if (!this.active) {
+            throw new IllegalStateException("Funds cannot be withdrawn from a closed account!");
+        }
+
+        if (amount > balance) {
+
+        }
+
+        raiseEvent(FundsWithdrawEvent.builder()
+                .id(this.id)
+                .amount(amount)
+                .build());
+    }
+
+    public void apply(FundsWithdrawEvent event) {
+        this.id = event.getId();
+        this.balance -= event.getAmount();
+    }
+
+    public void closedAccount() {
+        if (!this.active) {
+            throw new IllegalStateException("This bank account has already been closed!");
+        }
+        raiseEvent(AccountClosedEvent.builder()
+                .id(this.id)
+                .build());
+    }
+
+    public void apply(AccountClosedEvent event) {
+        this.id = event.getId();
+        this.active = false;
+    }
+
+}
