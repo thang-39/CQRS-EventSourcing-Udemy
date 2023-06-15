0107a377ab94c7693ed22f3ea960b815fb6a032f
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-15 11:10:01 +0700

    Event Consumer from Kafka

--- bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/consumers/EventConsumer.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/consumers/EventConsumer.java
@@ -1,7 +1,19 @@
 package com.techbank.account.query.infrastructure.consumers;
 
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawEvent;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 
 public interface EventConsumer {
-    void consume(@Payload)
+    void consume(@Payload AccountOpenedEvent event,
+                 Acknowledgment ack);
+    void consume(@Payload FundsDepositedEvent event,
+                 Acknowledgment ack);
+    void consume(@Payload FundsWithdrawEvent event,
+                 Acknowledgment ack);
+    void consume(@Payload AccountClosedEvent event,
+                 Acknowledgment ack);
 }
