0b9a811698800346ea125bf7e1a1266c9473390c
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-17 16:00:11 +0700

    add QueryHandler colleage

--- bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -2,7 +2,13 @@ package com.techbank.account.query.api.queries;
 
 import com.techbank.account.query.api.dto.EqualityType;
 import com.techbank.cqrs.core.queries.BaseQuery;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FindAccountWithBalanceQuery extends BaseQuery {
     private EqualityType equalityType;
     private double balance;
