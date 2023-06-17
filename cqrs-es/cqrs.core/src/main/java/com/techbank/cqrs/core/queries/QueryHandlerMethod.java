d742e609b84cbee76f0514b1d983edd0a1ef7891
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-17 16:12:17 +0700

    register Query from Dispatcher to QueryHandler

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/queries/QueryHandlerMethod.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/queries/QueryHandlerMethod.java
@@ -1,8 +1,10 @@
 package com.techbank.cqrs.core.queries;
 
+import com.techbank.cqrs.core.domain.BaseEntity;
+
 import java.util.List;
 
 @FunctionalInterface
 public interface QueryHandlerMethod<T extends BaseQuery> {
-    List<BaseQuery> handle(T query);
+    List<BaseEntity> handle(T query);
 }
